b988618ec876692e43d516463ee317bb0b90d609
Meltem Tokgöz <meltemtgz@gmail.com>
2020-01-04 19:36:38 +0300

    hotel pages
    Şuanda sadece tasarımlar eklendi.

---
+++ Desktop Application/EmployeeManagerPage.py
@@ -0,0 +1,158 @@
+try:
+    import tkinter as tk
+    from tkinter import *
+    import tkinter.messagebox as Messagebox
+except ImportError:
+    import Tkinter as tk
+    from Tkinter import *
+    import Tkinter.messagebox as Messagebox
+
+try:
+    import tkinter.ttk as ttk
+except ImportError:
+    import ttk
+
+import mysql.connector as mysql
+
+staffRoot = tk.Tk()
+staffRoot.geometry("1007x634+99+96")
+staffRoot.minsize(120, 1)
+staffRoot.maxsize(1370, 749)
+staffRoot.resizable(1, 1)
+staffRoot.title("Hotel Employees Page")
+
+def organizationPage():
+    import OrganizationPage
+
+def extraServicePage():
+    import ExtraServicePage
+
+def roomPage():
+    import HotelRoomPage
+
+
+'''SIDE BAR'''
+employee = Button(staffRoot, text="EMPLOYEES", font=(
+    "bold", 10), bg="#80ff00")
+employee.place(relx=0.028, rely=0.091, height=24, width=127)
+
+room = Button(staffRoot, text="ROOMS", font=(
+    "bold", 10), bg="#d9d9d9",command = roomPage)
+room.place(relx=0.028, rely=0.163, height=24, width=127)
+
+extra_services = Button(staffRoot, text="EXTRA SERVICES", font=(
+    "bold", 10), bg="#d9d9d9",command = extraServicePage )
+extra_services.place(relx=0.028, rely=0.236, height=24, width=127)
+
+organizations = Button(staffRoot, text="ORGANIZATIONS", font=(
+    "bold", 10), bg="#d9d9d9", command= organizationPage)
+organizations.place(relx=0.028, rely=0.308, height=24, width=127)
+
+TSeparator1 = ttk.Separator(staffRoot)
+TSeparator1.place(relx=0.179, rely=0.032, relheight=0.82)
+TSeparator1.configure(orient="vertical")
+
+TSeparator2 = ttk.Separator(staffRoot)
+TSeparator2.place(relx=0.188, rely=0.354, relwidth=0.780)
+
+'''INPUT TEXTS'''
+emp_id = Label(staffRoot, text="ID: ", font=("bold", 9))
+emp_id.place(relx=0.201, rely=0.065, height=21, width=24)
+
+e_emp_id = Entry(staffRoot)
+e_emp_id.place(relx=0.236, rely=0.065, height=20, relwidth=0.039)
+
+firstname = Label(staffRoot, text="First Name: ", font=("bold", 9))
+firstname.place(relx=0.285, rely=0.065, height=21, width=69)
+
+e_firstname = Entry(staffRoot)
+e_firstname.place(relx=0.360, rely=0.065, height=20, relwidth=0.150)
+
+lastname = Label(staffRoot, text="Last Name: ", font=("bold", 9))
+lastname.place(relx=0.520, rely=0.065, height=21, width=68)
+
+e_lastname = Entry(staffRoot)
+e_lastname.place(relx=0.600, rely=0.065, height=20, relwidth=0.150)
+
+telephone = Label(staffRoot, text="Phone: ", font=("bold", 9))
+telephone.place(relx=0.760, rely=0.065, height=21, width=46)
+
+e_telephone = Entry(staffRoot)
+e_telephone.place(relx=0.810, rely=0.065, height=20, relwidth=0.130)
+
+email = Label(staffRoot, text="Email: ", font=("bold", 9))
+email.place(relx=0.201, rely=0.113, height=21, width=41)
+
+e_email = Entry(staffRoot)
+e_email.place(relx=0.256, rely=0.113, height=20, relwidth=0.254)
+
+passwrd = Label(staffRoot, text="Password: ", font=("bold", 9))
+passwrd.place(relx=0.520, rely=0.113, height=19, width=60)
+
+e_password = Entry(staffRoot)
+e_password.place(relx=0.585, rely=0.113, height=20, relwidth=0.110)
+
+confirm_passwrd = Label(staffRoot, text="Confirm Password: ", font=("bold", 9))
+confirm_passwrd.place(relx=0.700, rely=0.113, height=21, width=109)
+
+e_confirm_passwrd = Entry(staffRoot)
+e_confirm_passwrd.place(relx=0.810, rely=0.113, height=20, relwidth=0.130)
+
+address = Label(staffRoot, text="Address: ", font=("bold", 9))
+address.place(relx=0.201, rely=0.160, height=21, width=54)
+
+e_address = Text(staffRoot)
+e_address.place(relx=0.256, rely=0.160, relheight=0.100, relwidth=0.35)
+
+salary = Label(staffRoot, text=" Salary: ", font=("bold", 9))
+salary.place(relx=0.610, rely=0.161, height=21, width=40)
+
+e_salary = Entry(staffRoot)
+e_salary.place(relx=0.660, rely=0.161, height=20, relwidth=0.049)
+
+'''COMBOBOX'''
+emp_type = Label(staffRoot, text="Employee Type: ", font=("bold", 9))
+emp_type.place(relx=0.720, rely=0.158, height=21, width=90)
+
+box_value = StringVar()
+e_emp_type = ttk.Combobox(
+    staffRoot, textvariable=box_value, state='readonly')
+e_emp_type.place(relx=0.810, rely=0.158, relheight=0.033, relwidth=0.130)
+
+e_emp_type['values'] = ('Normal', 'Termal', 'Bungalow')
+e_emp_type.current(0)
+
+'''OPERATION BUTTONS'''
+add = Button(staffRoot, text="Add", font=(
+    "italic", 10), bg="#d9d9d9")
+add.place(relx=0.337, rely=0.291, height=24, width=97)
+
+update = Button(staffRoot, text="Update", font=(
+    "italic", 10), bg="#d9d9d9")
+update.place(relx=0.437, rely=0.291, height=24, width=99)
+
+delete = Button(staffRoot, text="Delete", font=(
+    "italic", 10), bg="#d9d9d9")
+delete.place(relx=0.536, rely=0.291, height=24, width=97)
+
+get = Button(staffRoot, text="Get Employee", font=(
+    "italic", 10), bg="#d9d9d9")
+get.place(relx=0.635, rely=0.291, height=24, width=97)
+
+'''LIST OUTPUT'''
+manager = Label(staffRoot, text="Manager Table", font=("Comic Sans MC", 9))
+manager.place(relx=0.480, rely=0.36, height=31, width=90)
+
+manager_list = ttk.Treeview(staffRoot, height=10, columns=5)
+manager_list.grid(row=4, column=0)
+
+manager_list.place(relx=0.189, rely=0.415, relheight=0.18, relwidth=0.700)
+
+emp = Label(staffRoot, text="Employee Table", font=("Comic Sans MC", 9))
+emp.place(relx=0.480, rely=0.60, height=31, width=90)
+
+emp_list = ttk.Treeview(staffRoot, height=10, columns=5)
+emp_list.grid(row=4, column=0)
+
+emp_list.place(relx=0.189, rely=0.66, relheight=0.18, relwidth=0.700)
+tk.mainloop()
---
+++ Desktop Application/HotelRoomPage.py
@@ -0,0 +1,140 @@
+try:
+    import tkinter as tk
+    from tkinter import *
+    import tkinter.messagebox as Messagebox
+except ImportError:
+    import Tkinter as tk
+    from Tkinter import *
+    import Tkinter.messagebox as Messagebox
+
+try:
+    import tkinter.ttk as ttk
+except ImportError:
+    import ttk
+
+import mysql.connector as mysql
+
+hotelRoomRoot = tk.Tk()
+hotelRoomRoot.geometry("1007x634+99+96")
+hotelRoomRoot.minsize(120, 1)
+hotelRoomRoot.maxsize(1370, 749)
+hotelRoomRoot.resizable(1, 1)
+hotelRoomRoot.title("Hotel Room Page")
+
+def organizationPage():
+    import OrganizationPage
+
+def employeePage():
+    import EmployeeManagerPage
+
+def extraServicePage():
+    import ExtraServicePage
+
+'''SIDE BAR'''
+employee = Button(hotelRoomRoot, text="EMPLOYEES", font=(
+    "bold", 10), bg="#d9d9d9", command=employeePage)
+employee.place(relx=0.028, rely=0.091, height=24, width=127)
+
+room = Button(hotelRoomRoot, text="ROOMS", font=(
+    "bold", 10), bg="#80ff00")
+room.place(relx=0.028, rely=0.163, height=24, width=127)
+
+extra_services = Button(hotelRoomRoot, text="EXTRA SERVICES", font=(
+    "bold", 10), bg="#d9d9d9", command= extraServicePage)
+extra_services.place(relx=0.028, rely=0.236, height=24, width=127)
+
+organizations = Button(hotelRoomRoot, text="ORGANIZATIONS", font=(
+    "bold", 10), bg="#d9d9d9", command= organizationPage)
+organizations.place(relx=0.028, rely=0.308, height=24, width=127)
+
+TSeparator1 = ttk.Separator(hotelRoomRoot)
+TSeparator1.place(relx=0.179, rely=0.032, relheight=0.82)
+TSeparator1.configure(orient="vertical")
+
+TSeparator2 = ttk.Separator(hotelRoomRoot)
+TSeparator2.place(relx=0.188, rely=0.284, relwidth=0.635)
+
+'''INPUT TEXTS'''
+hotel_id = Label(hotelRoomRoot, text="ID: ", font=("bold", 9))
+hotel_id.place(relx=0.199, rely=0.063, height=21, width=23)
+
+e_hotel_id = Entry(hotelRoomRoot)
+e_hotel_id.place(relx=0.228, rely=0.063, height=20, relwidth=0.034)
+
+room_info = Label(hotelRoomRoot, text="Room Info: ", font=("bold", 9))
+room_info.place(relx=0.275, rely=0.063, height=21, width=68)
+
+e_room_info = Entry(hotelRoomRoot)
+e_room_info.place(relx=0.348, rely=0.063, relheight=0.08, relwidth=0.330)
+
+price = Label(hotelRoomRoot, text="Price: ", font=("bold", 9))
+price.place(relx=0.690, rely=0.063, height=21, width=38)
+
+e_price = Entry(hotelRoomRoot)
+e_price.place(relx=0.740, rely=0.063, height=20, relwidth=0.120)
+
+
+capacity = Label(hotelRoomRoot, text="Capacity: ", font=("bold", 9))
+capacity.place(relx=0.460, rely=0.158, height=21, width=58)
+
+e_capacity = Text(hotelRoomRoot)
+e_capacity.place(relx=0.520, rely=0.158, height=20, relwidth=0.044)
+
+room_number = Label(hotelRoomRoot, text="Room Number: ", font=("bold", 9))
+room_number.place(relx=0.570, rely=0.158, height=21, width=91)
+
+e_room_number  = Text(hotelRoomRoot)
+e_room_number.place(relx=0.660, rely=0.158,height=20, relwidth=0.044)
+
+'''COMBOBOX'''
+
+status = Label(hotelRoomRoot, text="Status: ", font=("bold", 9))
+status.place(relx=0.258, rely=0.158, height=21, width=44)
+
+box_value = StringVar()
+e_status = ttk.Combobox(
+    hotelRoomRoot, textvariable=box_value, state='readonly')
+e_status.place(relx=0.310, rely=0.158, relheight=0.033, relwidth=0.142)
+
+e_status['values'] = ('Normal', 'Termal', 'Bungalow')
+e_status.current(0)
+
+room_type = Label(hotelRoomRoot, text="Room Type: ", font=("bold", 9))
+room_type.place(relx=0.705, rely=0.158, height=21, width=72)
+
+box_value = StringVar()
+e_room_type = ttk.Combobox(
+    hotelRoomRoot, textvariable=box_value, state='readonly')
+e_room_type.place(relx=0.780, rely=0.158, relheight=0.033, relwidth=0.082)
+
+e_room_type['values'] = ('Normal', 'Termal', 'Bungalow')
+e_room_type.current(0)
+
+
+'''OPERATION BUTTONS'''
+add = Button(hotelRoomRoot, text="Add", font=(
+    "italic", 10), bg="#d9d9d9")
+add.place(relx=0.337, rely=0.221, height=24, width=97)
+
+update = Button(hotelRoomRoot, text="Update", font=(
+    "italic", 10), bg="#d9d9d9")
+update.place(relx=0.437, rely=0.221, height=24, width=99)
+
+delete = Button(hotelRoomRoot, text="Delete", font=(
+    "italic", 10), bg="#d9d9d9")
+delete.place(relx=0.536, rely=0.221, height=24, width=97)
+
+get = Button(hotelRoomRoot, text="Get Room", font=(
+    "italic", 10), bg="#d9d9d9")
+get.place(relx=0.635, rely=0.221, height=24, width=97)
+
+'''LIST OUTPUT'''
+rooms = Label(hotelRoomRoot, text="Room Table", font=("Comic Sans MC", 9))
+rooms.place(relx=0.480, rely=0.29, height=31, width=66)
+
+room_list = ttk.Treeview(hotelRoomRoot, height=10, columns=5)
+room_list.grid(row=4, column=0)
+
+room_list.place(relx=0.189, rely=0.340, relheight=0.38, relwidth=0.600)
+
+tk.mainloop()
