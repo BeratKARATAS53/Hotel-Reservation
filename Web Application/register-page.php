6a5c03775f0d44ddedbdaf5cee3dc169cd78253b
OkCat <48531104+BeratKARATAS53@users.noreply.github.com>
2020-01-19 17:01:01 +0300

    Css Styles Added

--- Web Application/register-page.php
+++ Web Application/register-page.php
@@ -4,62 +4,47 @@
 <head>
   <meta charset="utf-8">
   <title>Register Page</title>
+  <link rel="stylesheet" href="css/style.css" />
 </head>
 
 <body>
-  <center>
-    <p>REGISTER FORM</p>
-  </center>
-  <form id="form1" name="form1" action="register-func.php" method="post">
-    <table width="500" border="0" align="center">
-      <tbody>
-        <tr>
-          <td width="%25">First Name</td>
-          <td width="%25"><input type="text" name="first" id="first"></td>
-          <td width="%25">Last Name</td>
-          <td width="%25"><input type="text" name="last" id="last"></td>
-        </tr>
-        <tr>
-          <td width="96">Email</td>
-          <td width="300"><input type="text" name="mail" id="mail"></td>
-          <td width="96">Address</td>
-          <td width="153"><input type="text" name="address" id="address"></td>
-        </tr>
-        <tr>
-          <td width="96">Şifre</td>
-          <td><input type="text" name="pass" id="pass"></td>
-          <td width="196">Şifre (Tekrardan)</td>
-          <td><input type="text" name="confirm-pass" id="confirm-pass"></td>
-        </tr>
-        <tr>
-          <td width="96">Phone</td>
-          <td width="100"><input type="text" name="phone" id="phone"></td>
-          <td width="96">Age</td>
-          <td width="25"><input type="text" name="age" id="age"></td>
-        </tr>
-        <tr>
-          <td width="96">Username</td>
-          <td width="153"><input type="text" name="username" id="username"></td>
-          <td width="96">Money</td>
-          <td width="153"><input type="text" name="money" id="money"></td>
-        </tr>
-        <tr>
-          <td width="96">Hotel Name</td>
-          <td width="153"><input type="text" name="hotel-name" id="hotel-name"></td>
-          <td width="96">Person Type</td>
-          <td width="153"><input type="text" name="type" id="type"></td>
-        </tr>
-        <tr>
-          <td>&nbsp;</td>
-          <td><input type="submit" name="submit" id="submit" value="SAVE"></td>
-        </tr>
-      </tbody>
-    </table>
-  </form>
-  <center>
-    <p>&nbsp;</p>
-    <p><a href="login-page.php">Are You Member ?, Log In</a></p>
-  </center>
+  <div class="form">
+    <center class="title">
+      <p>REGISTER FORM</p>
+    </center>
+    <form id="form1" name="form1" action="register-func.php" method="post">
+      <input type="text" name="first" id="first" placeholder="First Name" />
+      <input type="text" name="last" id="last" placeholder="Last Name" />
+      <input type="email" name="mail" id="mail" placeholder="Mail" />
+      <input type="text" name="address" id="address" placeholder="Address" />
+      <input type="password" name="pass" id="pass" placeholder="Password" />
+      <input type="password" name="confirm-pass" id="confirm-pass" placeholder="Confirm Password" />
+      <input type="text" name="phone" id="phone" placeholder="Telephone" />
+      <input type="text" name="age" id="age" placeholder="Age" />
+      <input type="text" name="username" id="username" placeholder="Username" />
+      <input type="text" name="money" id="money" placeholder="Money" />
+      <input type="text" name="hotel-name" id="hotel-name" placeholder="Hotel Name" /><br>
+      <h3>Person Type</h3>
+      <label class="container">Customer
+        <input type="radio" name="type" id="customer" value="customer">
+        <span class="checkmark"></span>
+      </label>
+      <label class="container">Employee
+        <input type="radio" name="type" id="employee" value="employee">
+        <span class="checkmark"></span>
+      </label>
+      <label class="container">Manager
+        <input type="radio" name="type" id="manager" value="manager">
+        <span class="checkmark"></span>
+      </label>
+      <label>&nbsp;</label>
+      <input type="submit" name="submit" id="submit" value="SAVE">
+    </form>
+    <center>
+      <p>&nbsp;</p>
+      <p><a href="login-page.php">Are You Member ?, Log In</a></p>
+    </center>
+  </div>
 </body>
 
 </html>
