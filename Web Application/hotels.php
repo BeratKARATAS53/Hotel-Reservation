7aa0dff01781a5084bbd3d7c69274af063ca3d46
OkCat <48531104+BeratKARATAS53@users.noreply.github.com>
2020-01-28 15:58:54 +0300

    Searching Added

--- Web Application/hotels.php
+++ Web Application/hotels.php
@@ -1,19 +1,18 @@
 <?php
 require('db.php');
 ?>
-<!DOCTYPE html>
 <html>
-<title>Rooms</title>
+<title>Hotels</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 <body>
     <?php
-    if ($_SESSION['login_user'] == null) {
-        echo $_SESSION['login_user'], 1;
+    if ($_SESSION['email'] != null) {
+        echo $_SESSION['email'];
         include("navbar-login.php");
     } else {
-        echo $_SESSION['login_user'], 2;
+        echo $_SESSION['email'];
         include("navbar-not-login.php");
     }
     ?>
@@ -23,38 +22,40 @@ require('db.php');
             <h2 style="width:80%; margin: auto; margin-top: 50">Filters</h2>
         </div>
         <div class="w3-container">
-
-            <h2 style="width:80%; margin: auto; margin-top: 50">Rooms</h2>
+            <h2 style="width:80%; margin: auto; margin-top: 50">Hotels</h2>
             <?php
             $search_value = $_POST["search"];
             if ($search_value != '') {
-                $sel_query = "SELECT * FROM room USE INDEX (room_search_index) WHERE room_info LIKE '%$search_value%' ORDER BY id desc";
+                $sel_query = "SELECT * FROM hotel USE INDEX (hotel_search_index) WHERE name LIKE '%$search_value%' ORDER BY id desc";
             } else {
-                $sel_query = "SELECT * FROM room ORDER BY id desc";
+                $sel_query = "SELECT * FROM hotel ORDER BY id desc";
             }
             $result = mysqli_query($baglanti, $sel_query);
             while ($row = mysqli_fetch_assoc($result)) { ?>
                 <div style="width:80%; font-size: 15; margin: auto; margin-top: 50">
                     <header class="w3-container w3-light-grey">
-                        <h4> <?php echo $row["room_info"] ?> </h4>
+                        <h4> <?php echo $row["name"] ?> </h4>
                     </header>
-                    <p><?php echo "Room Price: ", $row["room_price"], " $" . "<br/>",
-                            "Room Number: ",
-                            $row["room_number"] . "<br/>",
-                            "Room Status: ",
-                            $row["status"] . "<br/>",
-                            "Room Capacity: ",
-                            $row["capacity"],
-                            " Person" . "<br/>" ?></p>
+                    <p><?php echo "Address: ", $row["address"], " $" . "<br/>",
+                            "Phone Number: ",
+                            $row["telephone"] . "<br/>",
+                            "Stars: ",
+                            $row["star"] . "<br/>",
+                            "Hotel Type: ",
+                            $row["hotel_type"] . "<br/>",
+                            "Hotel Info: ",
+                            $row["info"] . "<br/>"
+                        ?></p>
                     <hr>
                     <a class="w3-button w3-block" style="background: linear-gradient(to right, #7bff61, #93f9b9)" href="room-detail.php?id=<?php echo $row["id"]; ?>"> Details </a>
                     <hr style="border: 1px solid black;">
                 </div>
                 <br />
-            <?php  } ?>
+            <?php  }
+            ?>
+
         </div>
     </div>
-
 </body>
 
 </html>
