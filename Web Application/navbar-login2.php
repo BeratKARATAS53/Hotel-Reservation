3dfc3423cb1661ecf9ca2bbdbc44f249a47127f5
meltemtokgoz <meltemtgz@gmail.com>
2020-01-29 14:03:47 +0300

    profile-page sayfası css

--- Web Application/navbar-login2.php
+++ Web Application/navbar-login2.php
@@ -8,6 +8,7 @@ include('db.php');
     <meta charset="utf-8">
     <title>Member Page</title>
     <link rel="stylesheet" href="css/navbar.css" />
+	<link rel="stylesheet" href="css/style.css" />
 </head>
 
 <body>
@@ -19,6 +20,17 @@ include('db.php');
 
         <a style="float:right" href="logout.php">Log Out</a>
         <a class="active" style="float:right" href="profil-page.php"><?php echo $_SESSION['email']; ?></a>
+		
+		<form style="float:right; margin-right: 20px" method="post">
+            <input type="text" name="search" placeholder="Search">
+            <input type="submit" name="submit" value="Search" style="width: 25%">
+        </form>
+		
+	</div>
+	
 </body>
 
 </html>
+
+
+
