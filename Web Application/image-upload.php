7722624c3d13c9cdf700bcc0eb82e901f0884753
meltemtokgoz <meltemtgz@gmail.com>
2020-01-29 17:34:01 +0300

    Update image-upload.php

--- Web Application/image-upload.php
+++ Web Application/image-upload.php
@@ -59,6 +59,7 @@
 		}
 		// Check if $uploadOk is set to 0 by an error
 		if ($uploadOk == 0) {
+			echo "Sorry, Your file is not available for upload.";
 			// if everything is ok, try to upload file
 		} else {
 			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
