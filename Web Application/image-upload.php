f56819cb0a61a57d127a69157f5bdf03fdd93730
OkCat <48531104+BeratKARATAS53@users.noreply.github.com>
2020-01-29 15:16:22 +0300

    Image Upload Updated

--- Web Application/image-upload.php
+++ Web Application/image-upload.php
@@ -1,85 +1,60 @@
-<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"> </script>
-<script type="text/javascript">
-    var config = {
-        apiKey: "AIzaSyBPvVIQ12PN6hWW_eE-5D3FaViinPVfbjQ",
-        authDomain: "pet-adomption.firebaseapp.com",
-        databaseURL: "https://pet-adomption.firebaseio.com",
-        projectId: "pet-adomption",
-        storageBucket: "pet-adomption.appspot.com",
-        messagingSenderId: "640709970982",
-        appId: "1:640709970982:web:483fae29d6df77bfce2ae7",
-        measurementId: "G-BGEJRHMQZE"
-    };
-    firebase.initializeApp(config);
-    var selectedFile;
-
-    function getfile() {
-        var pic = document.getElementById("photo");
-
-        // selected file is that file which user chosen by html form 
-        selectedFile = pic.files[0];
-
-        // make save button disabled for few seconds that has id='submit_link' 
-        document.getElementById('submit_link').setAttribute('disabled', 'true');
-        myfunction(); // call below written function 
+<?php
+$target_dir = "C:\AppServ\www\css\image\img-";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if (
+    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif"
+) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    // if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
 
-    function myfunction() {
-        // select unique name for everytime when image uploaded 
-        // Date.now() is function that give current timestamp 
-        var name = "123" + Date.now();
-
-        // make ref to your firebase storage and select images folder 
-        var storageRef = firebase.storage().ref('/hotel_reservation_images/' + name);
-
-        // put file to firebase  
-        var uploadTask = storageRef.put(selectedFile);
-
-        // all working for progress bar that in html 
-        // to indicate image uploading... report 
-        uploadTask.on('state_changed', function(snapshot) {
-            var progress =
-                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            var uploader = document.getElementById('uploader');
-            uploader.value = progress;
-            switch (snapshot.state) {
-                case firebase.storage.TaskState.PAUSED:
-                    console.log('Upload is paused');
-                    break;
-                case firebase.storage.TaskState.RUNNING:
-                    console.log('Upload is running');
-                    break;
-            }
-        }, function(error) {
-            console.log(error);
-        }, function() {
-
-            // get the uploaded image url back 
-            uploadTask.snapshot.ref.getDownloadURL().then(
-                function(downloadURL) {
-
-                    document.getElementById('submit_link').removeAttribute('disabled');
-                });
-        });
-    };
-</script>
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
-
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style.css" />
-</head>
 
 <body>
 
-    <form>
-        <p>Resim Yüklenirken Kayıt Butonu 1-2 Saniye Devre Dışı Kalmaktadır.</p>
-        <input id="photo" class="file" type="file" name="userfile" value="" onchange="getfile()">
-        <button id="submit_link" type="submit" name="button">Save</button>
-        </input>
+    <form method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" name="upload" id="upload" value="Upload Image">
     </form>
+
 </body>
 
 </html>
